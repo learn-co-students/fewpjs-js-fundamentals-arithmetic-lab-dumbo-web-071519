23d863af2657b7d6bb6fd226ed157f61a706e2fe
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-09-01 20:08:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+let random = (Math.random() * 10) + 1
+const mod = 25 % 21;
+const max = Math.max(1,2,3,20);
 // Write your code here
