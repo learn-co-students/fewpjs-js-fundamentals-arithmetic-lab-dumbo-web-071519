fa3c396412587f17b23981c1273703ae994a9891
Lilaro <larochelle.lisa@gmail.com>
2019-09-05 16:27:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let num1 = 2
+let num2 = 31
 
-// Write your code here
+const multiply = num1 * num2 
+const random = Math.floor(Math.random()) * Infinity
+const mod = 24 % 10 
+const max = Math.max(1, 4, 20)
